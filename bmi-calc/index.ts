1b75c6c00bca40a3877a85170264052170c8eede
Shehan Kulathilake <shehanhere@gmail.com>
2023-04-29 11:55:17 +0530

    modified dummy application

--- bmi-calc/index.ts
+++ bmi-calc/index.ts
@@ -9,12 +9,14 @@ const weight = 190;
 const weightUnit = WeightUnits.POUNDS;
 const heightUnit = LengthUnits.CENTIMETER;
 
-function calculateBMI(){
+(function calculateBMI(){
+
+    console.log("Welcome to DEMO BMI Calculator ");
+    console.log("We will use the following measurements.");
+    console.log( `Height: ${height + heightUnit.toString()} and Weight ${weight + weightUnit.toString()}`);
 
     const heightInMeters = converter.LengthConverter.convert(height, heightUnit, LengthUnits.METER);
     const weightInKilos = converter.MassConverter.convert(weight, weightUnit, WeightUnits.KILOGRAM);
 
-    return weightInKilos/heightInMeters^2;
-}
-
-console.log(calculateBMI());
+    console.log(`Calculated BMI is ${weightInKilos/heightInMeters^2}`);
+})()
